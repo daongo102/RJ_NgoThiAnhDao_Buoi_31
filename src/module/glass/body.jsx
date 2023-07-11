0508d22c43be5c1824573904721a483fb2f2116f
NgoThiAnhDao <anhdaongo1092@gmail.com>
2023-07-11 22:12:46 +0700

    update code

--- src/module/glass/body.jsx
+++ src/module/glass/body.jsx
@@ -1,118 +1,102 @@
 import React, { Component } from "react";
-import Style from "./style.module.css";
 import { Glasses } from "./glasses";
 
 export default class Body extends Component {
     state = {
-        glass: "./img/v2.png",
+        glass: "./img/v5.png",
+        nameG: "PRADA P8750",
+        priceG: "40",
+        descG: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip.",
     };
-    changeGlass = (glass) => {
+    
+    changeGlass = (url, name, price, des) => {
         return () => {
-            this.setState({ glass: "./img/" + glass + ".png" });
+            this.setState({
+                glass: url,
+                nameG: name,
+                priceG: price,
+                descG: des,
+            });
         };
+    };
+
+    renderListGlasses = () => {
+        const result = Glasses.map((item) => {
+            return (
+                <div className="col-4 mb-5" key={item.id}>
+                    <button
+                        onClick={this.changeGlass(
+                            item.url,
+                            item.name,
+                            item.price,
+                            item.desc,
+                        )}
+                    >
+                        <img style={{ width: "100%" }} src={item.url} />
+                    </button>
+                </div>
+            );
+        });
+        return result;
     };
 
     render() {
         return (
             <div>
-                <div className={Style.BgImg}>
-                    <div className="container my-5">
-                        <div className="row">
-                            <div className="col-4">
-                                <img
+                <div className="container my-5">
+                    <div className="row">
+                        <div
+                            className="col-4"
+                            style={{ position: "relative", padding: 0 }}
+                        >
+                            <img
+                                style={{
+                                    width: "100%",
+                                }}
+                                src="./img/model.jpg"
+                            />
+                            <img
+                                style={{
+                                    width: "50%",
+                                    position: "absolute",
+                                    top: "30%",
+                                    left: "50%",
+                                    transform: "translate(-50%,-50%)",
+                                }}
+                                src={this.state.glass}
+                            />
+                            <div
+                                className="titleG"
+                                style={{
+                                    background: "rgba(0, 0, 0, 0.5)",
+                                    position: "absolute",
+                                    bottom: 0,
+                                    left: 0,
+                                    width: "100%",
+                                    height: "35%",
+                                    padding: "0.5rem 1rem",
+                                    color: "white",
+                                }}
+                            >
+                                <h4 style={{ marginBottom: "0.5rem" }}>
+                                    {this.state.nameG}
+                                </h4>
+                                <p
                                     style={{
-                                        width: "100%",
-                                        position: "relative",
+                                        marginBottom: "0.5rem",
+                                        color: "yellow",
                                     }}
-                                    src="./img/model.jpg"
-                                />
-                                <div>
-                                    <img
-                                        style={{
-                                            width: "15%",
-                                            position: "absolute",
-                                            top: "37%",
-                                            left: "13.4%",
-                                        }}
-                                        src={this.state.glass}
-                                    />
-                                </div>
+                                >
+                                    {this.state.priceG}$
+                                </p>
+                                <p style={{ fontSize: "0.8rem" }}>
+                                    {this.state.descG}
+                                </p>
                             </div>
-                            <div className="col-8">
-                                <div className="row">
-                                    <div className="col-4">
-                                        <button onClick={this.changeGlass("v1")}>
-                                            <img
-                                                style={{ width: "100%" }}
-                                                src="./img/v1.png"
-                                            />
-                                        </button>
-                                    </div>
-                                    <div className="col-4">
-                                        <button onClick={this.changeGlass("v2")}>
-                                            <img
-                                                style={{ width: "100%" }}
-                                                src="./img/v2.png"
-                                            />
-                                        </button>
-                                    </div>
-                                    <div className="col-4">
-                                        <button onClick={this.changeGlass("v3")}>
-                                            <img
-                                                style={{ width: "100%" }}
-                                                src="./img/v3.png"
-                                            />
-                                        </button>
-                                    </div>
-                                    <div className="col-4 my-5">
-                                        <button onClick={this.changeGlass("v4")}>
-                                            <img
-                                                style={{ width: "100%" }}
-                                                src="./img/v4.png"
-                                            />
-                                        </button>
-                                    </div>
-                                    <div className="col-4 my-5">
-                                        <button onClick={this.changeGlass("v5")}>
-                                            <img
-                                                style={{ width: "100%" }}
-                                                src="./img/v5.png"
-                                            />
-                                        </button>
-                                    </div>
-                                    <div className="col-4 my-5">
-                                        <button onClick={this.changeGlass("v6")}>
-                                            <img
-                                                style={{ width: "100%" }}
-                                                src="./img/v6.png"
-                                            />
-                                        </button>
-                                    </div>
-                                    <div className="col-4">
-                                        <button onClick={this.changeGlass("v7")}>
-                                            <img
-                                                style={{ width: "100%" }}
-                                                src="./img/v7.png"
-                                            />
-                                        </button>
-                                    </div>
-                                    <div className="col-4">
-                                        <button onClick={this.changeGlass("v8")}>
-                                            <img
-                                                style={{ width: "100%" }}
-                                                src="./img/v8.png"
-                                            />
-                                        </button>
-                                    </div>
-                                    <div className="col-4">
-                                        <button onClick={this.changeGlass("v9")}>
-                                            <img
-                                                style={{ width: "100%" }}
-                                                src="./img/v9.png"
-                                            />
-                                        </button>
-                                    </div>
-                                </div>
+                        </div>
+                        <div className="col-8">
+                            <div className="row">
+                                {this.renderListGlasses()}
                             </div>
                         </div>
                     </div>
